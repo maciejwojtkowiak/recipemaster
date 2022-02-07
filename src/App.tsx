199720a8d5eb460af5e80ef9e52ddf7bdb141e26
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-02-07 08:36:30 +0100

    filtered recipes by params

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import {useDispatch} from "react-redux"
+import {fetchRecipes} from "./store/recipe-action"
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import AddRecipe from './pages/AddRecipe';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import './App.css';
-import {useDispatch} from "react-redux"
-import {fetchRecipes} from "./store/recipe-action"
+import Detail from './pages/Detail';
 
 function App() {
   const dispatch = useDispatch()
@@ -21,6 +22,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/:recipeid' element={<Detail />} />
       <Route path='/addRecipe' element={<AddRecipe />} />
       <Route path='/Profile' element={<Profile />} />
       <Route path="/Register" element={<Register />} />
--- src/components/Home/RecipeItem.tsx
+++ src/components/Home/RecipeItem.tsx
@@ -1,14 +1,17 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Heading, Image, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 import { Recipe } from "../../shared/types/Recipe"
 
 
 const RecipeItem: React.FC<Recipe> = (props) => {
     return (
         <Box height="35vh" width="40vh" borderWidth="1px">
-            <h1>{props.title}</h1>
-            <p>{props.username}</p>
-            <p>{props.description}</p>
-            <p>{props.type}</p>
+            <Image height="20vh"></Image>
+            <Heading>{props.title}</Heading>
+            <Text>{props.username}</Text>
+            <Text>{props.description}</Text>
+            <Text>{props.type}</Text>
+            <Link to={`/${props.id}`}>Go</Link>
         </Box>
     )
 }
