6a814c3c4abb894ce661c58e225abdf1a754ac43
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-02-07 17:20:40 +0100

    hearts change colors on click

--- src/components/Home/RecipeItem.tsx
+++ src/components/Home/RecipeItem.tsx
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom"
 import { Recipe } from "../../shared/types/Recipe"
 import Breakfast from "../../images/Breakfast.jpg"
 import {FaHeart} from "react-icons/fa"
+import { useState } from "react"
 
 
 
 
 const RecipeItem: React.FC<Recipe> = (props) => {
+    const [isLiked, setIsLiekd] = useState<boolean>(false)
+    const onClickHandler = () => {
+        setIsLiekd(true)
+    }
     return (
         <Box height="40vh" width="40vh" borderWidth="1px" marginTop="2rem">
+            <Box position="relative">
+            <Icon onClick={onClickHandler} as={FaHeart} w={8} h={8} color={`${isLiked ? 'red.500' : 'white'}`} position="absolute" right="2%" top="4%" />
             <Image height="20vh" width="100%" src={Breakfast}></Image>
-            <Icon as={FaHeart} size="24px" color="red.500" />
+            </Box>
             <Box  marginLeft="1rem" marginTop="1rem">
                 <Heading font-weight="3rem">{props.title}<Text marginLeft="0.5rem" fontWeight="normal" fontSize="1rem" as="span">{props.type}</Text></Heading>
                 <Text fontSize="1.1rem"><Text fontSize="0.8rem" as="span">Author: </Text>{props.username}</Text>
