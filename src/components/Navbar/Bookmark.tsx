85e62266ae6583af52c8f8eeeef10d436811ba44
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-02-08 23:55:28 +0100

    added current number of liked items

--- src/components/Navbar/Bookmark.tsx
+++ src/components/Navbar/Bookmark.tsx
@@ -1,4 +1,4 @@
-import { Icon, IconButton, Menu, MenuButton, MenuItem, MenuList, Button, Box } from "@chakra-ui/react"
+import { Icon, IconButton, Menu, MenuButton, MenuItem, MenuList, Button, Box, Text } from "@chakra-ui/react"
 import {FiBookmark} from 'react-icons/fi'
 import { useSelector, useDispatch } from "react-redux"
 import { RootState } from "../../store/store"
@@ -8,12 +8,25 @@ import React from "react"
 const Bookmark = () => {
     const dispatch = useDispatch()
     const likedItems = useSelector((state: RootState) => state.recipe.likedRecipes.recipes)
+    const totalAmount = useSelector((state: RootState) => state.recipe.likedRecipes.totalAmount)
     const onDeleteHandler = (e: React.FormEvent, id: number) => {
         dispatch(recipeAction.deleteLikedRecipe(id))
     }
 
     return ( 
-        <Menu>
+        <Box position="relative">
+            <Menu>
+            <Text 
+            position="absolute" 
+            top="0%" right="0%" 
+            bg="white" borderRadius="50%" 
+            fontSize="0.8rem" color="black"
+            paddingLeft="0.4rem" 
+            paddingRight="0.4rem"
+            fontWeight="700"
+            zIndex="10">
+                {totalAmount}
+            </Text>
             <MenuButton 
                 background="none"
                 as={IconButton}
@@ -30,7 +43,9 @@ const Bookmark = () => {
             </MenuList>
 
             
-        </Menu>
+             </Menu>
+        </Box>
+
     )
 }
 
