bd0b044d258e2c39e474c4c00f193bbb24713fe6
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-02-24 17:20:51 +0100

    filters are menu now

--- src/components/Filters/FiltersBox.tsx
+++ src/components/Filters/FiltersBox.tsx
@@ -1,11 +1,35 @@
-import { Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Grid,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import FilterType from "./FilterType";
+import React, { useState } from "react";
 
 const FilterBox = () => {
+  const [filtersAreShown, setFiltersAreShown] = useState<boolean>(false);
+
+  const showFiltersHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setFiltersAreShown(true);
+  };
+
   return (
-    <Grid placeItems="center" templateColumns="repeat(3, 1fr)">
-      <FilterType />
-    </Grid>
+    <Box position="relative" height="100%" width="100%" zIndex="10">
+      <Box position="fixed" top="40%">
+        <Menu>
+          <MenuButton>Show Filters</MenuButton>
+          <MenuList marginLeft="1rem">
+            <Grid overflowY="scroll" maxH="30vh" marginLeft="1.5rem">
+              <FilterType />
+            </Grid>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Box>
   );
 };
 
