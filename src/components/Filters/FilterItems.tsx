f8e560e80a4075b4195310eec2a3d32fb526b0da
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-03-02 17:00:34 +0100

    added smooth transition in pages

--- src/components/Filters/FilterItems.tsx
+++ src/components/Filters/FilterItems.tsx
@@ -35,7 +35,9 @@ const FilterItems: React.FC<FuncProps> = (props) => {
 
   return (
     <React.Fragment>
-      <Checkbox _notFirst={{ marginTop: "2rem" }}>{props.filterTitle}</Checkbox>
+      <Text _notFirst={{ marginTop: "2rem" }} width="10vw">
+        {props.filterTitle}
+      </Text>
       <Stack pl={6} mt={1} spacing={1}>
         {props.options.map((option) => {
           return (
--- src/components/wrappers/PageAnimationWrapper.tsx
+++ src/components/wrappers/PageAnimationWrapper.tsx
@@ -2,11 +2,21 @@ import { motion } from "framer-motion";
 import React from "react";
 
 const animation = {
-  scale: 0.5,
+  visible: {
+    transition: {
+      duration: 0.6,
+    },
+    opacity: 1,
+  },
+  hidden: { opacity: 0 },
 };
 
 const PageAnimationWrapper: React.FC = (props) => {
-  return <motion.div animate={animation}>{props.children}</motion.div>;
+  return (
+    <motion.div initial="hidden" animate="visible" variants={animation}>
+      {props.children}
+    </motion.div>
+  );
 };
 
 export default PageAnimationWrapper;
