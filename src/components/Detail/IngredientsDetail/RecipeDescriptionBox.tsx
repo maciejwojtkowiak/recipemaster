ca87e4725e75877caea958a7b6d2c94fce867eee
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-03-05 15:37:09 +0100

    created files for each box of recipe preparation details

---
+++ src/components/Detail/IngredientsDetail/RecipeDescriptionBox.tsx
@@ -0,0 +1,5 @@
+const RecipeDescriptionBox = () => {
+  return <div></div>;
+};
+
+export default RecipeDescriptionBox;
---
+++ src/components/Detail/IngredientsDetail/RecipeIngredientsBox.tsx
@@ -0,0 +1,5 @@
+const RecipeIngredientsBox = () => {
+  return <div></div>;
+};
+
+export default RecipeIngredientsBox;
