81774c7ade45a9054789ffa2ac17ccf858298b4a
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-03-12 10:31:58 +0100

    started styling notification box

--- src/components/UI/Notification.tsx
+++ src/components/UI/Notification.tsx
@@ -7,7 +7,22 @@ const Notification = () => {
     (state: RootState) => state.ui.notificationIsShown
   );
   const notification = useSelector((state: RootState) => state.ui.notification);
-  return <Box>{notificationIsShown && notification}</Box>;
+  return (
+    <Box
+      position="fixed"
+      bottom="1%"
+      left="50%"
+      h="10vh"
+      w="20vw"
+      transform="translate(-50%, -50%)"
+      zIndex="10"
+      bgColor="white"
+      borderColor="orange.400"
+      borderWidth="5px"
+    >
+      {notificationIsShown && notification}
+    </Box>
+  );
 };
 
 export default Notification;
