0a711599f1f668ef1b34b5f801b4324b9dab30ee
Maciej Wojtkowiak <mac.woj1000@gmail.com>
2022-03-31 19:07:24 +0100

    deleted useless box in commentItem

--- src/components/Detail/Comments/CommentList/CommentItem.tsx
+++ src/components/Detail/Comments/CommentList/CommentItem.tsx
@@ -15,29 +15,28 @@ const CommentItem: React.FC<funcProps> = (props) => {
       border="1px"
       borderColor="orange.400"
       marginTop="1rem"
+      marginBottom="1rem"
       paddingTop="2rem"
       paddingBottom="2rem"
     >
-      <Grid maxW="100%" placeItems="center" marginLeft="2rem">
-        <Flex maxW="100%">
-          <Grid maxW="100%" placeItems="center" marginRight="2rem">
-            <Flex alignItems="center" flexDir="column">
-              <Image boxSize="5rem" src={defaultUser} />
-              <Text marginTop="1rem" fontWeight="700">
-                {props.comment.user}
-              </Text>
-            </Flex>
-          </Grid>
-          <Grid maxW="100%" templateRows="max-content 1fr">
-            <Text fontWeight="700" marginBottom="0.5rem" fontSize="1.6rem">
-              {props.comment.title}
+      <Flex marginLeft="2rem">
+        <Grid maxW="100%" placeItems="center" marginRight="2rem">
+          <Flex alignItems="center" flexDir="column">
+            <Image boxSize="5rem" src={defaultUser} />
+            <Text marginTop="1rem" fontWeight="700">
+              {props.comment.user}
             </Text>
-            <Text maxW="30vw" marginLeft="0.5rem">
-              {props.comment.content}
-            </Text>
-          </Grid>
-        </Flex>
-      </Grid>
+          </Flex>
+        </Grid>
+        <Grid maxW="100%" templateRows="max-content 1fr">
+          <Text fontWeight="700" marginBottom="0.5rem" fontSize="1.6rem">
+            {props.comment.title}
+          </Text>
+          <Text maxW="30vw" marginLeft="0.5rem">
+            {props.comment.content}
+          </Text>
+        </Grid>
+      </Flex>
     </Flex>
   );
 };
